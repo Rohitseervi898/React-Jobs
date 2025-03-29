9034201d88377403b14ba33fc1f818e5e8b8b027
Rohitseervi898 <rohitseervi8866@gmail.com>
2025-03-29 23:42:51 +0530

    Single Job output

--- src/component/jobListing.jsx
+++ src/component/jobListing.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import {FaMapMarker} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
 
-const jobsListing = ({job}) => {
+const jobsListing = ({job ,isHome=false}) => {
     // console.log(job);
     const [showDesc,setShowDesc]=useState(false);
     let description= job.description;
@@ -35,7 +35,7 @@ const jobsListing = ({job}) => {
                 {job.location}
             </div>
             <Link
-                to={`./jobs/${job.id}`}
+                to={isHome ?`./jobs/${job.id}`:`./${job.id}`}
                 className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
             >
                 Read More
