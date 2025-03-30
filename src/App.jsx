5236fa1b2752f13f1d3ab1014a32887f7de641c6
Rohitseervi898 <rohitseervi8866@gmail.com>
2025-03-30 20:58:19 +0530

    1. Form Submission 2. Pass Function as Prop 3. POST request to add job 4. delete job button 5.  delete request to remove 6. react toastify 7. edit job Page 8. Update Form submission 9. PUT Request to update job

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,60 @@
-import {Route, createBrowserRouter, createRoutesFromElements,RouterProvider,
-} from 'react-router-dom'
+import {Route, createBrowserRouter, createRoutesFromElements,RouterProvider,} from 'react-router-dom'
 import React, { useState } from 'react'
 import HomePages from './Pages/HomePages'
 import MainLayout from './layouts/MainLayout'
 import JobsPage from './Pages/JobsPage'
 import NotFoundPage from './Pages/NotFoundPage'
 import JobPage,{jobloader} from './Pages/JobPage'
+import AddJobPage from './Pages/AddJobPage'
+import EditJobPage from './Pages/EditJobPage'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path='/' element={<MainLayout />}>
-    <Route index element={<HomePages />} />
-    <Route path='/jobs' element={<JobsPage />} />
-    <Route path='/jobs/:id' element={<JobPage />} loader={jobloader}/>
-    <Route path='*' element={<NotFoundPage />} />
-    
-  </Route>
-  
-)
-)
+
 const App = () => {
+  // Add new Job
+  const addJob = async(newJob)=>{
+    const res =await fetch('/api/jobs',{
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    });
+    return;
+  };
+
+  //delete Job
+  const deleteJob=async(id)=>{
+    const res =await fetch(`/api/jobs/${id}`,{
+      method: 'DELETE',
+    });
+    return;  
+  }
+  //update job
+  const updateJob=async(job)=>{
+    const res =await fetch(`/api/jobs/${job.id}`,{
+      method: 'PUT',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job),
+    });
+    return; 
+  }
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePages />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/add-job' element={<AddJobPage AddJobSubmit={addJob} />} />
+      <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit={updateJob} />} loader={jobloader}/>
+      <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobloader}/>
+      <Route path='*' element={<NotFoundPage />} />
+      
+    </Route>
+    
+  )
+  )
   return <RouterProvider router={router} />;
 }
 export default App
